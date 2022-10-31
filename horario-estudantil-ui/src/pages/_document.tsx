a9deb99a1415213829acc3ab582eb81c4ef7135e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2022-10-31 19:53:09 -0400

    feat(ui): init icons

--- horario-estudantil-ui/src/pages/_document.tsx
+++ horario-estudantil-ui/src/pages/_document.tsx
@@ -10,7 +10,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* next-pwa */}
         <meta name="application-name" content={APP_NAME} />
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -21,56 +20,13 @@ export default function Document() {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
 
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-
-        <meta name="theme-color" content="#000000" />
-
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content={APP_BASE_URL} />
         <meta name="twitter:title" content={APP_TITLE} />
         <meta name="twitter:description" content={APP_DESCRIPTION} />
         <meta
           name="twitter:image"
-          content={`${APP_BASE_URL}/icons/android-chrome-192x192.png`}
+          content={`${APP_BASE_URL}/icons/android-icon-192x192.png`}
         />
         <meta name="twitter:creator" content="@antunes_fk" />
         <meta property="og:type" content="website" />
@@ -80,25 +36,94 @@ export default function Document() {
         <meta property="og:url" content={APP_BASE_URL} />
         <meta
           property="og:image"
-          content={`${APP_BASE_URL}/icons/apple-touch-icon.png`}
+          content={`${APP_BASE_URL}/icons/apple-icon-152x152.png`}
         />
-
-        {/* <!-- apple splash screen images --> */}
-
-        {/* <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> */}
-
-        {/* end next-pwa */}
 
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/icons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/icons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/icons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/icons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/icons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/icons/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2E7D32" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta
+          name="msapplication-TileImage"
+          content="/icons/ms-icon-144x144.png"
+        />
+
+        <meta name="theme-color" content="#2E7D32" />
       </Head>
       <body>
         <Main />
